755e7b4ab2cd199fb473b3ca21e023d3235381ff
Vincent Wong <vinny.wong@outlook.com>
2022-12-15 14:49:35 -0500

    Committing missing files for hosting

--- web_project/urls.py
+++ web_project/urls.py
@@ -1,4 +1,4 @@
-"""powerlifting_app URL Configuration
+"""web_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path, include 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', include('core.urls'))
-
 ]
