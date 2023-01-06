85ed20b1d294e721a7d9dcbc0d956f65b91a75d4
Vincent Wong <vinny.wong@outlook.com>
2023-01-06 11:12:51 -0800

    Setting static root to filesystem path

--- powerlifting_app/settings.py
+++ powerlifting_app/settings.py
@@ -135,6 +135,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT = '/var/www/static'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "powerlifting_frontend/build/static")
 ]
