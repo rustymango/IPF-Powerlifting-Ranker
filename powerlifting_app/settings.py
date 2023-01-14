414ea773adba4c7e83c8f021120e0a55fec797aa
Vincent Wong <vinny.wong@outlook.com>
2023-01-13 22:50:11 -0500

    Configuring missing dependencies for deployment

--- powerlifting_app/settings.py
+++ powerlifting_app/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 REST_FRAMEWORK = {'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny']}
